e4f363fef16496c790db81897bc3e7513056f752
Neeyu571 <neeyuvaidhya6879@gmail.com>
2026-06-11 18:44:13 +0530

    Add calculation logic and update input and marksheet

--- input.py
+++ input.py
@@ -1,17 +1,45 @@
-class inputAll:
-	def __init__(self,msnum,name,mname,fname,roll,cno,stream,school_n):
-		self.msnum=int(input("Enter the Ms no.:"))
-		self.name=input("Enter name:")
-		self.mname=input("Enter Mother name:")
-		self.fname=input("Enter father name:")
-		self.roll=int(input("Enter roll no.:"))
-		self.cno=int(input("Enter Center no.:"))
-		self.stream=input("Enter Stream")
-		self.school_n=input("Enter School name:")
+import random
+class inputAll():
+    def __init__(self):
+        self.name = input("Enter name: ")
+        self.mname = input("Enter Mother name: ")
+        self.fname = input("Enter father name: ")
+        self.stream = input("Enter Stream: ")
+        self.school_n = input("Enter School name: ")
+        self.ms_no = random.randrange(1000000,9999999)
+        self.rno = random.randrange(1000000,9999999)
+        self.cno = random.randrange(10000,99999)
+        self.reg = random.randrange(1000000,9999999)
+        #self.dob = date(input("Enter Your DOB:"))
+        self.s1m=int(input("Enter the Marks of Hindi"))
+        self.s2m=int(input("Enter the Marks of English"))
+        self.s3m=int(input("Enter the Marks of Physics"))
+        self.s4m=int(input("Enter the Marks of Chemistry"))
+        self.s5m=int(input("Enter the Marks of Maths"))
 
-	def displayAll(self,msnum,name,mname,fname,roll,cno,stream,school_n):
-		print(displayAll())
 
-inp=inputAll()
+    def displayAll(self):
+        print(f"{self.school_n:^95}")
+        msc = f"Ms. No.: {self.ms_no}"
+        print(f"\n{msc:>100}")
+
+        #print(f{:>80 "Ms. No.":{self.ms_no}})
+        print(f"Name: {self.name}")
+        print(f"Mother Name: {self.mname}")
+        print(f"Father Name: {self.fname}")
+            
+        print(f"\nRoll no.:{self.rno}"," "*19,f"\tCenter no.:{self.cno}")
+        print(f"\nRegistration no.:{self.reg}"," "*15,f"Faculty:{self.stream}\n")
+        print(f"College nmae/Schol Name: \n{self.school_n}\n")
+        
+        print("Subjects"," "*4,"Marks\n")
+        print(f"Hindi"," "*7,self.s1m)
+        print(f"English"," "*5,self.s2m)
+        print(f"Physics"," "*5,self.s3m)
+        print(f"Chemistry"," "*3,self.s4m)
+        print(f"Maths"," "*7,self.s5m)
+    
+        
+
+inp = inputAll()
 inp.displayAll()
-
---
+++ marksheet.py
@@ -0,0 +1,2 @@
+class marksheet():
+	
